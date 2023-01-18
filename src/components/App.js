064455bc42e3ab07dc1f1cddae7c6a9294c117a0
deepakyadav121999 <dy44444221@gmail.com>
2023-01-18 15:48:00 +0530

    fpp

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const[number1,setnumber1]=useState(0);
+  const[number2,setnumber2]=useState(0);
+  const[total,setTotal]= useState(number1 + number2);
+  function ip1(e){
+   
+    setnumber1(+e.target.value)
+    setTotal(number1 +number2);
+ 
+
+  }
+  function ip2(e){
+    setnumber2(+e.target.value)
+  
+      setTotal(number1 +number2);
+   
+   
+  }
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' type='number' onChange={ip1} />
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' type='number' onChange={ip2}/> 
       
-      <p id='result'></p>
+      <p id='result' >{total}</p>
     </div>
   )
 }
